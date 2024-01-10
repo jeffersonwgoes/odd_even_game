1e904ea3745a92cc35c0cdd1258eb5ac30013930
Jefferson Goes <jeffersonwgoes@gmail.com>
2024-01-09 23:03:40 -0300

    Added second scoreboard and almost finished the game

--- python/main.py
+++ python/main.py
@@ -2,6 +2,9 @@ import random
 import json
 import os
 
+
+LINE_DIV="=================================================="
+
 def getRandomNumber():
     random.seed()
     return random.randrange(10)
@@ -18,27 +21,34 @@ def getMachineName():
 def loadScoreBoard():
     print("ScoreBoard")
     if not os.path.exists('scoreboard.txt'):
-        return {}
-    
+        return {}, []
+    d = {}
     with open('scoreboard.txt', 'r+') as filep:
-        return json.load(filep)
+        d = json.load(filep)
 
-def saveScoreBoard(scoreboard):
-    with open('scoreboard.txt', 'w+') as score:
-        jsonScore = json.dump(scoreboard, score)
+    wins = dict(sorted(d["wins"].items(), key=lambda item: item[1], reverse=True))
+    diff = d["diff"]
+    return wins, diff
+
+def saveScoreBoard(wins, diff):
+    score = {"wins": wins, "diff": diff}
+    with open('scoreboard.txt', 'w+') as filepe:
+        jsonScore = json.dump(score, filepe)
 
     
 
 def main():
-    scoreboard = loadScoreBoard()
-    print(scoreboard)
-    pName = 'Jeff' #input('Your name: ')
+    print(LINE_DIV)
+    wScore, dScore = loadScoreBoard()
+    print(wScore, dScore)
+    print(LINE_DIV)
+    pName = input('Your name: ')
     mName = getMachineName()
     print(f"{pName} vs {mName}")
     pScore = 0
     mScore = 0
     while pScore < 3 and mScore < 3:
-        playerV = 2#int(input("Enter a number between 0 and 9: "))
+        playerV = 2# int(input("Enter a number between 0 and 9: "))
         choice = 'Even'#input("Choose between Odd and Even: ")
         machineV = getRandomNumber()
         sum = machineV + playerV
@@ -55,20 +65,23 @@ def main():
         print(f"Overallscore: {pName}: {pScore} {mName}: {mScore}")
 
 
-
+    diff = pScore - mScore
     if pScore == 3:
+        dScore.append({pName: diff})
         winner = pName
+        if pName in wScore:
+            wScore[winner] += 1
+        else:
+            wScore[winner] = 1
     else:
         winner = mName
+        diff *= -1
 
-    print(f"{winner} won the game")
+    print(LINE_DIV)
+    print(f"{winner} won the game. Games diff:  {diff}")
+    print(LINE_DIV)
 
-    if winner in scoreboard:
-        scoreboard[winner] += 1
-    else:
-        scoreboard[winner] = 1
-
-    saveScoreBoard(scoreboard)
+    saveScoreBoard(wScore, dScore)
 
 
 if __name__ == "__main__":
