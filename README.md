835b64fb7782481623ffe03457e6022bae828f21
jwfg <jeffersonwgoes@gmail.com>
2024-01-10 23:19:13 -0300

    Added the scoreboards enhancement
    Now the scoreboards work and cut the limit the result to 10 highest scores;
    The nice print is implemented;
    Changed to playable game;

---
+++ README.md
@@ -0,0 +1,24 @@
+# Odd Even game
+
+A simple odd even game to play a little with python and c++
+
+## Requirements
+
+### Game
+- The game will be played player agains machine
+- The player inform his name
+- The machine will pick a random name
+- The game starts asking a number to the player;
+- Then asking odd or even
+- The machine will play a random number
+- Each round values 1 point
+- Wins who has 10 points
+- At the end, the player should enter to the scoreboard if:
+    - Has a higher number of victories
+    - Has a bigger gap then the other players in the scoreboard
+
+### Machine
+- The machine needs to take name and number from a random generator web site
+
+### Scoreboard
+- The scoreboard should keep only player names, no machine allowed
--- python/main.py
+++ python/main.py
@@ -19,37 +19,61 @@ def getMachineName():
     return "John Doe"
 
 def loadScoreBoard():
-    print("ScoreBoard")
     if not os.path.exists('scoreboard.txt'):
         return {}, []
     d = {}
     with open('scoreboard.txt', 'r+') as filep:
         d = json.load(filep)
 
-    wins = dict(sorted(d["wins"].items(), key=lambda item: item[1], reverse=True))
-    diff = d["diff"]
-    return wins, diff
+    return d["wins"], d["diff"]
 
 def saveScoreBoard(wins, diff):
+    __MAX_SIZE_SCOREBOARD = 10
+    # order the dict before save
+    wins = dict(sorted(wins.items(), key=lambda item: item[1], reverse=True))
+    wins = dict(list(wins.items())[:__MAX_SIZE_SCOREBOARD])
+    diff.sort(key=lambda item: item[list(item)[0]], reverse=True)
+    diff = diff[:__MAX_SIZE_SCOREBOARD]
     score = {"wins": wins, "diff": diff}
-    with open('scoreboard.txt', 'w+') as filepe:
-        jsonScore = json.dump(score, filepe)
+    with open('scoreboard.txt', 'w+') as filep:
+        jsonScore = json.dump(score, filep)
 
-    
+def printScoreboard(wins, diff):
+    print(LINE_DIV)
+    print("                   Scoreboard")
+    print(LINE_DIV)
+    print("                     Winners")
+    print(LINE_DIV)
+    print("Name\t\t\tScore")
+    print(LINE_DIV)
+    for item in wins:
+        print(f"{item}\t\t\t{wins[item]}")
+
+    print(LINE_DIV)
+    print("           Bigger diff between opponent")
+    print(LINE_DIV)
+    print("Name\t\t\tDiff")
+    print(LINE_DIV)
+    for item in diff:
+        key = list(item)[0]
+        print(f"{key}\t\t\t{item[key]}")
+    print(LINE_DIV)
 
 def main():
-    print(LINE_DIV)
     wScore, dScore = loadScoreBoard()
-    print(wScore, dScore)
+    printScoreboard(wScore, dScore)
     print(LINE_DIV)
-    pName = input('Your name: ')
+    pName = input('Enter your name: ')
     mName = getMachineName()
     print(f"{pName} vs {mName}")
     pScore = 0
     mScore = 0
-    while pScore < 3 and mScore < 3:
-        playerV = 2# int(input("Enter a number between 0 and 9: "))
-        choice = 'Even'#input("Choose between Odd and Even: ")
+    round = 0
+    __DIFF_TO_WIN = 10
+    while pScore < __DIFF_TO_WIN and mScore < __DIFF_TO_WIN:
+        print(LINE_DIV)
+        choice = input("Choose between Odd and Even: ")
+        playerV = int(input("Enter a number between 0 and 9: "))
         machineV = getRandomNumber()
         sum = machineV + playerV
         result = oddEven(sum)
@@ -66,7 +90,7 @@ def main():
 
 
     diff = pScore - mScore
-    if pScore == 3:
+    if pScore == __DIFF_TO_WIN:
         dScore.append({pName: diff})
         winner = pName
         if pName in wScore:
