a1f6e3bf782ed02e2ed6ae5a4f9f90599059f53c
Jefferson Goes <jeffersonwgoes@gmail.com>
2025-07-28 13:01:33 -0300

    Added loading score from file

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -17,6 +17,7 @@ class FileManagement {
         void print_file(void);
         void save(void);
         void blabla(void);
+        std::vector<std::string> get_lines(void);
     private:
         std::string path;
         std::vector<std::string> lines;
@@ -61,24 +62,46 @@ void FileManagement::blabla(void)
     // }
 }
 
+std::vector<std::string> FileManagement::get_lines(void)
+{
+    return lines;
+}
+
+struct Score {
+    std::string name;
+    int score;
+};
+
 class Scoreboard {
-    public :
+    public:
         Scoreboard();
         void add_highscore(int score);
         bool is_highscore(int score);
-    private :
-        FileManagement *scores; 
+    private:
+        std::vector<Score> scores;      
+        FileManagement *file_scores;
+        Score parse_line(std::string);
+        
 };
 
 Scoreboard::Scoreboard()
 {
-    scores = new FileManagement("scoreboard.txt");
+    file_scores = new FileManagement("scoreboard.txt");
+    for(std::string line : file_scores->get_lines()) 
+    {
+        scores.push_back(parse_line(line));
+    }
 }
 
-void Scoreboard::add_highscore(int)
-{
+// void Scoreboard::is_highscore(int score)
+// {
+
+// }
+
+// void Scoreboard::add_highscore(int)
+// {
     
-}
+// }
 
 int main(void)
 {
